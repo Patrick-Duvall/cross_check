79a5aa736558c9fe4fab28c4ae2c4f82b7cb16b4
Patrick Duvall <duvallp208@gmail.com>
2019-04-01 17:01:30 -0600

    Add game stats tests. modify game stats test sample
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: Patrick-Duvall <duvallp208@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- test/games_statistics/games_statistics_test.rb
+++ test/games_statistics/games_statistics_test.rb
@@ -2,6 +2,58 @@ require "./test/test_helper"
 
 class GameStatistics < Minitest::Test
 
+  def setup
+    game_path = './data/game_sample.csv'
+      team_path = './data/team_info_sample.csv'
+      game_teams_path = './data/game_teams_stats_sample.csv'
 
+      locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+
+      @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  def test_returns_highest_total_score
+    assert_equal 7, @stat_tracker.highest_total_score
+  end
+
+  def test_returns_lowest_total_score
+    assert_equal 1, @stat_tracker.lowest_total_score
+  end
+
+  def test_returns_biggest_blowout
+    assert_equal 5, @stat_tracker.biggest_blowout
+  end
+
+  def test_returns_percentage_home_wins
+    assert_equal 0.5, @stat_tracker.percentage_home_wins
+  end
+
+  def test_returns_percentage_away_wins
+    assert_equal 0.5, @stat_tracker.percentage_away_wins
+  end
+
+  def test_returns_count_of_game_by_season
+    expected = {
+      '20122013' => 4,
+      '20152016' => 4
+                }
+    assert_equal expected, @stat_tracker.count_of_games_by_season
+  end
+
+  def test_returns_average_goals_per_game
+    assert_equal 4.38, @stat_tracker.average_goals_per_game
+  end
+
+  def test_returns_average_goals_by_season
+    expected = {
+      '20122013' => 5.5,
+      '20152016' => 3.25
+                }
+    assert_equal expected, @stat_tracker.average_goals_by_season
+  end
 
 end
