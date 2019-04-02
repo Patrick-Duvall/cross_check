18ee89f765d1e4f2a36e94a87928975408b770e4
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-02 13:50:10 -0600

    Add functionality to parse game team CSV
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: Patrick-Duvall <duvallp208@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,11 +2,12 @@ require "csv"
 require "./lib/game_stats"
 class StatTracker
   include GameStatistics
-  attr_reader :game_stats, :team_stats
+  attr_reader :game_stats, :team_stats, :game_teams
 
   def initialize
     @game_stats = {}
     @team_stats = {}
+    @game_teams = {}
   end
 
   def from_csv(locations)
@@ -17,7 +18,7 @@ class StatTracker
        home_goals: game[:home_goals],
        away_goals: game[:away_goals],
        outcome: game[:outcome]
-     }
+      }
     end
     team_table = CSV.table(locations[:teams], options = Hash.new)
     team_table.each do |team|
@@ -27,7 +28,17 @@ class StatTracker
        short_name: team[:shortname],
        team_name: team[:teamname],
        abbreviation: team[:abbreviation]
-     }
+      }
+    end
+    league_table = CSV.table(locations[:game_teams], options = Hash.new)
+    league_table.each do |game_stat|
+      @game_teams[game_stat[:team_id]] = {
+        game_id:   game_stat[:game_id],
+        home_away: game_stat[:hoa],
+        goals:     game_stat[:goals],
+        won:       game_stat[:won]
+      }
     end
   end
+
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -5,18 +5,18 @@ require "csv"
 class StatTrackerTest < Minitest::Test
 
   def setup
-    game_path = './data/sample/game_sample.csv'
-      team_path = './data/sample/team_info_sample.csv'
-      game_teams_path = './data/sample/game_teams_stats_sample.csv'
+    game_path       = './data/sample/game_sample.csv'
+    team_path       = './data/sample/team_info_sample.csv'
+    game_teams_path = './data/sample/game_teams_stats_sample.csv'
 
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
 
-      @stat_tracker = StatTracker.new
-      @stat_tracker.from_csv(locations)
+    @stat_tracker = StatTracker.new
+    @stat_tracker.from_csv(locations)
   end
 
   def test_it_exists
@@ -25,11 +25,13 @@ class StatTrackerTest < Minitest::Test
 
   def test_reads_csv_games
     expected = {2012030221=>{:season=>20122013, :home_goals=>3, :away_goals=>2, :outcome=>"home win OT"}, 2012030222=>{:season=>20122013, :home_goals=>5, :away_goals=>2, :outcome=>"home win REG"}, 2012030223=>{:season=>20122013, :home_goals=>1, :away_goals=>2, :outcome=>"away win REG"}, 2012030224=>{:season=>20122013, :home_goals=>4, :away_goals=>3, :outcome=>"home win OT"}, 2015030133=>{:season=>20152016, :home_goals=>1, :away_goals=>6, :outcome=>"away win REG"}, 2015030134=>{:season=>20152016, :home_goals=>2, :away_goals=>1, :outcome=>"home win REG"}, 2015030135=>{:season=>20152016, :home_goals=>0, :away_goals=>2, :outcome=>"away win REG"}, 2015030136=>{:season=>20152016, :home_goals=>0, :away_goals=>1, :outcome=>"away win REG"}}
- assert_equal expected, @stat_tracker.game_stats
+
+    assert_equal expected, @stat_tracker.game_stats
   end
 
   def test_reads_csv_teams
     expected = {1=>{:franchise_id=>23, :short_name=>"New Jersey", :team_name=>"Devils", :abbreviation=>"NJD"}, 4=>{:franchise_id=>16, :short_name=>"Philadelphia", :team_name=>"Flyers", :abbreviation=>"PHI"}, 26=>{:franchise_id=>14, :short_name=>"Los Angeles", :team_name=>"Kings", :abbreviation=>"LAK"}, 14=>{:franchise_id=>31, :short_name=>"Tampa Bay", :team_name=>"Lightning", :abbreviation=>"TBL"}}
+
     assert_equal expected, @stat_tracker.team_stats
   end
 
