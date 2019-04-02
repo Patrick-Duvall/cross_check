b90fd27a2760185532cc2bae988b0e554e143d58
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-02 17:07:42 -0600

    Change best offense to actually function, add worst offense method
    Co-authored-by: BrennanAyers <brennan.f.ayers@gmail.com>
    Co-authored-by: Patrick-Duvall <duvallp208@gmail.com>
    Co-authored-by: alexander-mathieu <alexander.mathieu@icloud.com>

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -5,15 +5,23 @@ module LeagueStatistics
   end
 
   def best_offense
-    best_team = @game_teams.values.max_by do |team|
-      x = team.reduce(0) do |sum, game|
-      #  require "pry"; binding.pry
-          sum += game[1][:goals]
+    best_team = @game_teams.max_by do |team_id, games|
+      x = games.reduce(0) do |sum, game|
+        sum += game[1][:goals]
       end
-      x.fdiv(team.count)
+      x.fdiv(games.count)
     end
-    @team_stats[best_team[:team_id]]
-    require "pry"; binding.pry
+    @team_stats[best_team[0]][:team_name]
   end
 
+  def worst_offense
+    worst_team = @game_teams.min_by do |team_id, games|
+      x = games.reduce(0) do |sum, game|
+        sum += game[1][:goals]
+      end
+      x.fdiv(games.count)
+    end
+    @team_stats[worst_team[0]][:team_name]
+  end
+  
 end
